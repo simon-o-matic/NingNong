9c86eff0b66a13afec1aecd66382380b9e58144f
Simon-o-Matic <simonraikallen@gmail.com>
2014-11-24 09:49:12 +1100

    Little code clean up.

--- ningnong.js
+++ ningnong.js
@@ -1,13 +1,35 @@
 //
 // Ning Nong
 //
+// A wrapper around Ping (on OS X) which makes it easier to see how things have gone
+// in a long running session.
+//
+// Architecture:
+// 
+// NingNong spawn a ping process and then listens for I/O from the process. Here are
+// the idiosyncracies.
+//
+// 	stdin: When all is good stdin gets the std ping "64 bytes... ip... ttl ... time" 
+//		string. Unfortunately when there is no response from the target machine
+//		(ie., you'd normally see a "timeout..." message) these are not available to 
+//		the underying stream on the 'data' event. They infact only arrive tacked onto
+//		the begining of the next success message - which is too late. I've tried to 
+//		get them out of the stream in various ways to no avail. So the code sets up
+//		its own timeout detection thread. This is sub optimal (and by no means 
+// 		accurate) but sufficient for the moment. Would love to get some pull requests
+//		that fix this.
+//
+//	stderr: When no connection is present ping emits a "Send to" or "No route" type
+//		strings to stderr. In this case there is also a return character (\n) on the 
+//		end which needs to be removed.
+//
 'use strict';
 
 var spawn 	= require('child_process').spawn;
-var Ning 	= require('./ning');
 var events 	= require('events');
-var nong 	= new events.EventEmitter();
 var util 	= require('util');
+
+var GRACE_PERIOD = 1.5; 	// seconds to wait over and above before we assume ther is a timeout
 
 kickItAllOff();
 
@@ -16,21 +38,11 @@ function kickItAllOff() {
 
 	var child = spawn('ping', ['-i ' + pauseTime, 'google.com']);
 
-	child.stdout.on('data', function(data) {
-		nong.emit("ping", data);
-	});
-
-	child.stderr.on('data', function(data) {
-		nong.emit("pong", data);
-	});
-
+	child.stdout.on('data', processPing);
+	child.stderr.on('data', processErrorPing);
 	child.on('close', function(code) {
-	    console.log('closing code: ' + code);
+	    console.log('Ping stopped: ' + code);
 	});
-
-	nong.on("pong", processErrorPing);
-	nong.on("ping", processPing);
-	nong.on("timeout", processTimeout);
 
 	waitForNoPing(pauseTime);
 }
@@ -48,12 +60,6 @@ function processTimeout(ping) {
 }
 
 function processPing(ping) {
-	// calc time
-	// parse
-	//if (/timeout/.exec(ping) || //route)
-
-	// timeout: timeout
-	// network down.
 	var duration = /time=([\S]+)/.exec(ping);
 
 	if (duration === null) { 		
@@ -61,7 +67,6 @@ function processPing(ping) {
 	} else {
 		if (pings == 1) util.print("\n");
 		util.print(">GOOD [" + (pings++) + "] " + duration[1] + "\r");
-//		return new Ning(Date.now(), true, duration);
 		timeouts = 1;
 		noRoutes = 1;
 
@@ -87,8 +92,8 @@ function processErrorPing(ePing) {
 }
 
 function waitForNoPing(time) {
-	setTimeout(function() {
+	setInterval(function() {
 		// Its only a timeout if we can contact the server
 		if (noRoutes < 2) processTimeout();
-	}, time * 1.5 * 1000);
+	}, (time + GRACE_PERIOD) * 1000);
 }
